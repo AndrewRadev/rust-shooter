87dfcc33732d4c0f5253c7a1e22120c0ed7ef202
Andrew Radev <andrey.radev@gmail.com>
2017-12-27 15:07:43 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+//! Basic hello world example.
+
+extern crate ggez;
+use ggez::conf;
+use ggez::event;
+use ggez::{Context, GameResult};
+use ggez::graphics;
+use std::env;
+use std::path;
+
+// First we make a structure to contain the game's state
+struct MainState {
+    text: graphics::Text,
+    frames: usize,
+}
+
+// Then we implement the `ggez:event::EventHandler` trait on it, which
+// requires callbacks for updating and drawing the game state each frame.
+//
+// The `EventHandler` trait also contains callbacks for event handling
+// that you can override if you wish, but the defaults are fine.
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 48)?;
+        let text = graphics::Text::new(ctx, "Hello world!", &font)?;
+
+        let s = MainState {
+            text: text,
+            frames: 0,
+        };
+        Ok(s)
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        // Drawables are drawn from their center.
+        let dest_point = graphics::Point2::new(self.text.width() as f32 / 2.0 + 10.0,
+                                               self.text.height() as f32 / 2.0 + 10.0);
+        graphics::draw(ctx, &self.text, dest_point, 0.0)?;
+        graphics::present(ctx);
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            println!("FPS: {}", ggez::timer::get_fps(ctx));
+        }
+        Ok(())
+    }
+}
+
+// Now our main function, which does three things:
+//
+// * First, create a new `ggez::conf::Conf`
+// object which contains configuration info on things such
+// as screen resolution and window title,
+// * Second, create a `ggez::game::Game` object which will
+// do the work of creating our MainState and running our game,
+// * then just call `game.run()` which runs the `Game` mainloop.
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("helloworld", "ggez", c).unwrap();
+
+    // We add the CARGO_MANIFEST_DIR/resources do the filesystems paths so
+    // we we look in the cargo project for files.
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        ctx.filesystem.mount(&path, true);
+    }
+
+    let state = &mut MainState::new(ctx).unwrap();
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
+}
