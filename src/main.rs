ad1707680eff3eb1753210d03b68fcc03f6146a3
Andrew Radev <andrey.radev@gmail.com>
2017-12-30 15:51:51 +0200

    Load resources, don't do anything with them quite yet

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,83 @@ use ggez::event;
 use ggez::{Context, GameResult};
 use ggez::graphics;
 use ggez::timer;
+use ggez::audio;
+use ggez::graphics::{Vector2, Point2};
 use std::env;
 use std::path;
 
-// First we make a structure to contain the game's state
+struct Assets {
+    ferris_normal_image: graphics::Image,
+    ferris_shooting_image: graphics::Image,
+    shot_image: graphics::Image,
+    font: graphics::Font,
+    shot_sound: audio::Source,
+    hit_sound: audio::Source,
+}
+
+impl Assets {
+    fn new(ctx: &mut Context) -> GameResult<Assets> {
+        let ferris_normal_image = graphics::Image::new(ctx, "/ferris-normal.png")?;
+        let ferris_shooting_image = graphics::Image::new(ctx, "/ferris-shooting.png")?;
+        let shot_image = graphics::Image::new(ctx, "/shot.png")?;
+
+        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 18)?;
+
+        let shot_sound = audio::Source::new(ctx, "/pew.ogg")?;
+        let hit_sound = audio::Source::new(ctx, "/boom.ogg")?;
+
+        Ok(Assets {
+            ferris_normal_image, ferris_shooting_image,
+            shot_image, font,
+            shot_sound, hit_sound,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Player {
+    pos: Point2,
+    velocity: Vector2,
+    bbox_size: f32,
+}
+
+impl Player {
+    fn new(pos: Point2) -> Self {
+        Player {
+            pos,
+            velocity: Vector2::new(0.0, 0.0),
+            bbox_size: 10.0,
+        }
+    }
+}
+
 struct MainState {
-    text: graphics::Text,
-    frames: usize,
-    xoffset: f32,
-    xdir: f32,
-    yoffset: f32,
-    ydir: f32,
+    assets: Assets,
+    player: Player,
     screen_width: u32,
     screen_height: u32,
 }
 
-// Then we implement the `ggez:event::EventHandler` trait on it, which
-// requires callbacks for updating and drawing the game state each frame.
-//
-// The `EventHandler` trait also contains callbacks for event handling
-// that you can override if you wish, but the defaults are fine.
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf", 48)?;
-        let text = graphics::Text::new(ctx, "Hello world!", &font)?;
+        let assets = Assets::new(ctx)?;
+        let screen_width = ctx.conf.window_mode.width;
+        let screen_height = ctx.conf.window_mode.height;
+
+        // Player starts in bottom-middle of the screen
+        let player_bbox_size = 10.0;
+        let player_pos = Point2::new(
+            (screen_width as f32) / 2.0 + player_bbox_size,
+            (screen_height as f32) - player_bbox_size
+        );
 
         let s = MainState {
-            text: text,
-            frames: 0,
-            xoffset: 0.0,
-            xdir: 1.0,
-            yoffset: 0.0,
-            ydir: 1.0,
+            assets: assets,
+            player: Player::new(player_pos),
             screen_width: ctx.conf.window_mode.width,
             screen_height: ctx.conf.window_mode.height,
         };
+
         Ok(s)
     }
 }
@@ -53,24 +95,7 @@ impl event::EventHandler for MainState {
         while timer::check_update_time(ctx, DESIRED_FPS) {
             let seconds = 1.0 / (DESIRED_FPS as f32);
 
-            if ((self.text.width() as f32) / 2.0 + 10.0 + self.xoffset) as u32 * 2 <= 0 {
-                self.xdir = 1.0;
-            }
 
-            if ((self.text.height() as f32) / 2.0 + 40.0 + self.yoffset) as u32 <= 0 {
-                self.ydir = 1.0;
-            }
-
-            if ((self.text.width() as f32) / 2.0 + 10.0 + self.xoffset) as u32 * 2 > self.screen_width {
-                self.xdir = -1.0;
-            }
-
-            if ((self.text.height() as f32) / 2.0 + 40.0 + self.yoffset) as u32 > self.screen_height {
-                self.ydir = -1.0;
-            }
-
-            self.xoffset += 100.0 * seconds * self.xdir;
-            self.yoffset += 100.0 * seconds * self.ydir;
         }
 
         Ok(())
@@ -78,30 +103,25 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
-        // Drawables are drawn from their center.
-        let dest_point = graphics::Point2::new(self.text.width() as f32 / 2.0 + 10.0 + self.xoffset,
-                                               self.text.height() as f32 / 2.0 + 10.0 + self.yoffset);
-        graphics::draw(ctx, &self.text, dest_point, 0.0)?;
+
+        //let pos = world_to_screen_coords(self.screen_width, self.screen_height, ferris.pos);
+        //let image = assets.actor_image(actor);
+        //let drawparams = graphics::DrawParam {
+        //    dest: pos,
+        //    rotation: actor.facing as f32,
+        //    offset: graphics::Point2::new(0.5, 0.5),
+        //    .. Default::default()
+        //};
+        //graphics::draw_ex(ctx, image, drawparams);
+
         graphics::present(ctx);
-        self.frames += 1;
-        if (self.frames % 100) == 0 {
-            println!("FPS: {}", ggez::timer::get_fps(ctx));
-        }
         Ok(())
     }
 }
 
-// Now our main function, which does three things:
-//
-// * First, create a new `ggez::conf::Conf`
-// object which contains configuration info on things such
-// as screen resolution and window title,
-// * Second, create a `ggez::game::Game` object which will
-// do the work of creating our MainState and running our game,
-// * then just call `game.run()` which runs the `Game` mainloop.
 pub fn main() {
     let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("helloworld", "ggez", c).unwrap();
+    let ctx = &mut Context::load_from_conf("shooter", "ggez", c).unwrap();
 
     // We add the CARGO_MANIFEST_DIR/resources do the filesystems paths so
     // we we look in the cargo project for files.
@@ -112,6 +132,7 @@ pub fn main() {
     }
 
     let state = &mut MainState::new(ctx).unwrap();
+
     if let Err(e) = event::run(ctx, state) {
         println!("Error encountered: {}", e);
     } else {
